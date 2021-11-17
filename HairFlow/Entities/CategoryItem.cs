94c61f6ad3cf0e43805dbda50879a43c0278c581
Daniel Jimenez <danyfebrero@gmail.com>
2021-11-17 13:45:36 -0500

    Datepicker fix

--- HairFlow/Entities/CategoryItem.cs
+++ HairFlow/Entities/CategoryItem.cs
@@ -10,6 +10,7 @@ namespace HairFlow.Entities
 {
     public class CategoryItem
     {
+        private DateTime _releaseDate = DateTime.MinValue;
         public int Id { get; set; }
         [Required]
         [StringLength(200, MinimumLength = 2)]
@@ -19,7 +20,18 @@ namespace HairFlow.Entities
         public int MadiaTypeId { get; set; }
         [NotMapped]
         public virtual ICollection<SelectListItem> MediaTypes { get; set; }
-        public DateTime DateTimeItemReleased { get; set; }
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}")]
+        public DateTime DateTimeItemReleased 
+        {
+            get 
+            {
+                return (_releaseDate == DateTime.MinValue) ? DateTime.Now : _releaseDate;
+            }
+            set
+            {
+                _releaseDate = value;
+            }
+        }
         [NotMapped]
         public int ContentId { get; set; }
     }
---
+++ HairFlow/wwwroot/js/DatePicker.js
@@ -0,0 +1,19 @@
+﻿$(function () {
+
+    function WireUpDatePicker()
+    {
+        const addMonths = 12;
+        var currentDate = new Date();
+
+         $('.datepicker').datepicker(
+                {
+                    dateFormat: 'yy-mm-dd',
+                    minDate: currentDate,
+                    maxDate: AddSubtractMonths(currentDate, addMonths)
+                }
+         );
+    }
+
+    WireUpDatePicker();
+
+});
