c5a2e76303f9ece1173034f7df0f15b89bfe7b82
Daniel Jimenez <danyfebrero@gmail.com>
2021-11-17 22:39:48 -0500

    registratio dialog

--- HairFlow/Controllers/UserAuthController.cs
+++ HairFlow/Controllers/UserAuthController.cs
@@ -65,5 +65,45 @@ namespace HairFlow.Controllers
                 return RedirectToAction("Index", "Home");
             }
         }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RegisterUser(RegistrationModel registrationModel)
+        {
+            registrationModel.RegistrationInValid = "true";
+
+            if (ModelState.IsValid)
+            {
+                ApplicationUser user = new ApplicationUser
+                {
+                    UserName = registrationModel.Email,
+                    Email = registrationModel.Email,
+                    PhoneNumber = registrationModel.PhoneNumber,
+                    FirstName = registrationModel.FirstName,
+                    LastName = registrationModel.LastName,
+                    Address1 = registrationModel.Address1,
+                    Address2 = registrationModel.Address2,
+                    ZipCode = registrationModel.ZipCode
+
+                };
+
+                var result = await _userManager.CreateAsync(user, registrationModel.Password);
+
+                if (result.Succeeded)
+                {
+                    registrationModel.RegistrationInValid = "";
+
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+
+                    return PartialView("_UserRegistrationPartial", registrationModel);
+                }
+
+                ModelState.AddModelError("","Registration attempt failed");
+
+            }
+            return PartialView("_UserRegistrationPartial", registrationModel);
+
+        }
     }
 }
--- README.md
+++ README.md
@@ -16,7 +16,7 @@ This project was built as Code Louisville C# 2 final project. As such, this proj
 - [x] Create a dictionary or list, populate it with several values, retrieve at least one value, and use it in your program
 - [ ] Implement a log that records errors, invalid inputs, or other important events and writes them to a text file
 - [x] Read data from an external file, such as text, JSON, CSV, etc and use that data in your application
-- [ ] Implement a regular expression (regex) to ensure a field either a phone number or an email address is always stored and displayed in the same format
+- [x] Implement a regular expression (regex) to ensure a field either a phone number or an email address is always stored and displayed in the same format
 - [x] Connect to an external/3rd party API and read data into your app
 - [x] Use a LINQ query to retrieve information from a data structure (such as a list or array) or file
 - [ ] Create 3 or more unit tests for your application
